326042b897425bf60692ad8ebf406fae56221228
Maverick Vitto <mavvitto@yahoo.com>
2023-10-23 13:28:27 -0600

    Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const names = [names]
+
+function writeCards(names) {
+    const thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+  
+    return thankYouMessages;
+  }
+  
+  const messages = writeCards(names);
+  
+  console.log(messages)
+
+function countDown() {
+    let countDown = 10
+    while (countDown >= 0) {
+        console.log(countDown--)
+    }
+    return countDown
+}
